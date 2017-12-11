e05e81cdac5e72852459b754b736cc657143c53a
Valerie Jane <valeriejanethoma@gmail.com>
2017-12-11 12:29:20 -0500

    add NavBar to site

---
+++ front-end/src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+	constructor(){
+		super();
+	}
+	render(){
+		return(
+			<h1>Hello</h1>
+		)
+	}
+}
+
+export default NavBar;
